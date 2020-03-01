384566291ec61991aedfb7e1f92ef916e3267035
Pzz0912 <pzz@onyx-international.com>
2020-03-01 18:59:43 +0800

    修改源码

--- android/src/main/java/com/alinz/parkerdan/shareextension/ShareModule.java
+++ android/src/main/java/com/alinz/parkerdan/shareextension/ShareModule.java
@@ -13,13 +13,27 @@ import android.net.Uri;
 
 import android.graphics.Bitmap;
 import java.io.InputStream;
+import android.util.Log;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Build;
+import android.text.TextUtils;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 
 public class ShareModule extends ReactContextBaseJavaModule {
 
-
+  private Context mContext;
   public ShareModule(ReactApplicationContext reactContext) {
       super(reactContext);
+      mContext = reactContext;
   }
 
   @Override
@@ -53,14 +67,34 @@ public class ShareModule extends ReactContextBaseJavaModule {
         if (type == null) {
           type = "";
         }
+
         if (Intent.ACTION_SEND.equals(action) && "text/plain".equals(type)) {
           value = intent.getStringExtra(Intent.EXTRA_TEXT);
         }
-        else if (Intent.ACTION_SEND.equals(action) && ("image/*".equals(type) || "image/jpeg".equals(type) || "image/png".equals(type) || "image/jpg".equals(type) ) ) {
+        else if (Intent.ACTION_SEND.equals(action) && 
+        ("image/*".equals(type) || 
+        "image/jpeg".equals(type) || 
+        "image/png".equals(type) || 
+        "image/jpg".equals(type) || 
+        "audio/mpeg".equals(type) || 
+        "video/mp4".equals(type) || 
+        "application/pdf".equals(type) || 
+        "application/msword".equals(type) || 
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet".equals(type) || 
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document".equals(type) || 
+        "application/vnd.ms-exce".equals(type) || 
+        "application/epub+zip".equals(type) ) ) {
+
           Uri uri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
-         value = "file://" + RealPathUtil.getRealPathFromURI(currentActivity, uri);
+          
+          if (Build.VERSION.SDK_INT >= 24) {
+            value = "file://" + getFilePathFromURI(uri);//新的方式
+          } else {
+            value = "file://" + RealPathUtil.getRealPathFromURI(currentActivity, uri);
+          }
 
        } else {
+         
          value = "";
        }
       } else {
@@ -73,4 +107,64 @@ public class ShareModule extends ReactContextBaseJavaModule {
 
       return map;
   }
+
+  public String getFilePathFromURI(Uri contentUri) {
+        File rootDataDir = mContext.getFilesDir();
+        String fileName = getFileName(contentUri);
+        if (!TextUtils.isEmpty(fileName)) {
+            File copyFile = new File(rootDataDir + File.separator + fileName);
+            copyFile(mContext, contentUri, copyFile);
+            return copyFile.getAbsolutePath();
+        }
+        return null;
+    }
+
+    public  String getFileName(Uri uri) {
+        if (uri == null) return null;
+        String fileName = null;
+        String path = uri.getPath();
+        int cut = path.lastIndexOf('/');
+        if (cut != -1) {
+            fileName = path.substring(cut + 1);
+        }
+        return fileName;
+    }
+
+    public void copyFile(Context context, Uri srcUri, File dstFile) {
+        try {
+            InputStream inputStream = context.getContentResolver().openInputStream(srcUri);
+            if (inputStream == null) return;
+            OutputStream outputStream = new FileOutputStream(dstFile);
+            copyStream(inputStream, outputStream);
+            inputStream.close();
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int copyStream(InputStream input, OutputStream output) throws Exception, IOException {
+        final int BUFFER_SIZE = 1024 * 2;
+        byte[] buffer = new byte[BUFFER_SIZE];
+        BufferedInputStream in = new BufferedInputStream(input, BUFFER_SIZE);
+        BufferedOutputStream out = new BufferedOutputStream(output, BUFFER_SIZE);
+        int count = 0, n = 0;
+        try {
+            while ((n = in.read(buffer, 0, BUFFER_SIZE)) != -1) {
+                out.write(buffer, 0, n);
+                count += n;
+            }
+            out.flush();
+        } finally {
+            try {
+                out.close();
+            } catch (IOException e) {
+            }
+            try {
+                in.close();
+            } catch (IOException e) {
+            }
+        }
+        return count;
+    }
 }
